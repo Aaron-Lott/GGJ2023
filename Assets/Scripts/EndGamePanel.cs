cc5718d90e9000c075a9ac1aea77e5cc1efb4917
Aaron Lott <aaronjameslott@gmail.com>
2023-02-05 17:53:41 +0000

    Add more story cards

--- Assets/Scripts/EndGamePanel.cs
+++ Assets/Scripts/EndGamePanel.cs
@@ -9,18 +9,23 @@ public class EndGamePanel : MonoBehaviour
     public TMP_Text firstMessage;
     public TMP_Text secondMessage;
 
-    public Animator animator;
-
-    private void Start()
+    private void OnEnable()
     {     
         GameManager.OnGameLost += ShowPanelLose;
         GameManager.OnGameWon += ShowPanelWin;
         GameManager.OnGameDraw += ShowPanelDraw;
     }
 
+    private void OnDisable()
+    {
+        GameManager.OnGameLost -= ShowPanelLose;
+        GameManager.OnGameWon -= ShowPanelWin;
+        GameManager.OnGameDraw -= ShowPanelDraw;
+    }
+
     public void ShowPanelLose(FamilyMemberData winningFamilyMember, FamilyMemberData losingFamilyMember)
     {
-        animator.SetTrigger("trigger");
+        GetComponent<Animator>()?.SetTrigger("trigger");
         firstMessage.text = "Seems you didn't quite fit in with the Wilotts this time around.\n\n After a trail period, they decided that maybe another family would be a better fit for you.";
         secondMessage.text = $"You especially didn't get along with {losingFamilyMember.FamilyMemberName}.\n\n Try to ensure you don't lower multiple family member's trust too low next time, and perhaps you'll settle in better...";
         StartCoroutine(ReturnToMainMenu());
@@ -28,7 +33,7 @@ public class EndGamePanel : MonoBehaviour
 
     public void ShowPanelWin(FamilyMemberData winningFamilyMember, FamilyMemberData losingFamilyMember)
     {
-        animator.SetTrigger("trigger");
+        GetComponent<Animator>()?.SetTrigger("trigger");
         firstMessage.text = $"After living with the Wilotts for some time, you've managed to build a strong relationship with {winningFamilyMember.FamilyMemberName}!\n\n Turns out {winningFamilyMember.secret}";
         secondMessage.text = $"Seems you didn't get along with {losingFamilyMember.FamilyMemberName}, so you may need to change your approach in future.\n\n Congrats on building a strong bond with {winningFamilyMember.FamilyMemberName}!";
         StartCoroutine(ReturnToMainMenu());
@@ -36,7 +41,7 @@ public class EndGamePanel : MonoBehaviour
 
     public void ShowPanelDraw(FamilyMemberData winningFamilyMember, FamilyMemberData losingFamilyMember)
     {
-        animator.SetTrigger("trigger");
+        GetComponent<Animator>()?.SetTrigger("trigger");
         firstMessage.text = "After striking a good balance with the family, you continue to make yourself at home with the Wilotts.";
         secondMessage.text = "You didn't manage to uncover any of the family member's secrets this time around, so you may need to change your approach in future.";
         StartCoroutine(ReturnToMainMenu());
